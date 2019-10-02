7a28ae4e8032a3098db350dfbc7e4de5a3abdc84
Andrew Herbig <notandrewherbig@gmail.com>
2019-10-02 00:06:33 -0400

    oh yeah remove these folders

--- includes/uplink.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <stddef.h>
-
-typedef signed long long ssize_t;
-
-extern int ptyps_socket(void);
-extern int ptyps_link(int sock, uint16_t port, char* host);
-extern int ptyps_out(int sock, char* text);
-extern ssize_t ptyps_recv(int sock, void* buffer, size_t size);
